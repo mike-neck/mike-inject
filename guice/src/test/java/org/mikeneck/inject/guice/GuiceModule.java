b6195ebdce75dc34e3245878e02558f835bb0dfe
mike_neck <jkrt3333@gmail.com>
2013-01-01 17:23:37 +0900

    pass Tck for Guice

--- guice/src/test/java/org/mikeneck/inject/guice/GuiceModule.java
+++ guice/src/test/java/org/mikeneck/inject/guice/GuiceModule.java
@@ -12,9 +12,12 @@ public class GuiceModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Car.class).to(Convertible.class);
+        requestStaticInjection(Convertible.class);
         bind(Seat.class);
         bind(Seat.class).annotatedWith(Drivers.class).to(DriversSeat.class);
         bind(Engine.class).to(V8Engine.class);
+        bind(SpareTire.class);
+        requestStaticInjection(SpareTire.class);
         bind(Tire.class).annotatedWith(Names.named("spare")).to(SpareTire.class);
     }
 }
